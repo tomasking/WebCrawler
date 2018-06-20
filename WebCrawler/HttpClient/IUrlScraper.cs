a8a1dd2e3e855954dc7826c6c1f67d2f24e97ea2
tomas-king <Tomas.King@moonpig.com>
2018-06-20 12:48:16 +0100

    Started implementing scraping of urls

--- WebCrawler/HttpClient/IUrlScraper.cs
+++ WebCrawler/HttpClient/IUrlScraper.cs
@@ -2,28 +2,9 @@
 {
 	using System;
 	using System.Collections.Generic;
-	using System.Text.RegularExpressions;
 
 	public interface IUrlScraper
 	{
 		List<string> ScrapeUrls(string webPageContent);
-	}
-
-	public class UrlScraper : IUrlScraper
-	{
-		public List<string> ScrapeUrls(string webPageContent)
-		{
-			var urls = new List<string>();
-			
-			MatchCollection m1 = Regex.Matches(webPageContent, @"<a\s+(?:[^>]*?\s+)?href=([""'])(.*?)\1", RegexOptions.Singleline);
-			foreach (Match m in m1)
-			{
-				string value = m.Groups[2].Value;
-				urls.Add(value);
-				
-			}
-
-			return urls;
-		}
 	}
 }
