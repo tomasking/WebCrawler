add5f3cc71c6d0a22bf10f3bc6265f63ab82f3a1
tomas-king <Tomas.King@moonpig.com>
2018-06-21 17:14:44 +0100

    Making concurrent

--- WebCrawler/Crawler/Strategies/BreadthFirstSearchStrategy.cs
+++ WebCrawler/Crawler/Strategies/BreadthFirstSearchStrategy.cs
@@ -1,5 +1,7 @@
 ﻿namespace WebCrawler.Crawler.Strategies
 {
+	using System;
+	using System.Collections.Concurrent;
 	using System.Collections.Generic;
 	using System.Threading.Tasks;
 	using UrlScraping;
@@ -15,18 +17,26 @@
 
 		public async Task<PageNode> Crawl(string rootDomain)
 		{
-			var visited = new HashSet<string>();
-
-			var queue = new Queue<PageNode>();
+			var visited = new HashSet<string>(); //TODO: not concurrent
+			
+			var queue = new ConcurrentQueue<PageNode>();
 
 			var seedNode = new PageNode("/");
 			queue.Enqueue(seedNode);
 
 			while (queue.Count > 0)
 			{
-				var currentNode = queue.Dequeue();
-				visited.Add(currentNode.Url);
+				await Process(rootDomain, queue, visited);
+			}
 
+			return seedNode;
+		}
+
+		private async Task Process(string rootDomain, ConcurrentQueue<PageNode> queue, HashSet<string> visited)
+		{
+			if (queue.TryDequeue(out var currentNode))
+			{
+				visited.Add(currentNode.Url);
 				var childUrls = await _urlScraper.ScrapeUrls(rootDomain, currentNode.Url);
 
 				foreach (var childUrl in childUrls)
@@ -38,10 +48,44 @@
 						queue.Enqueue(childNode);
 						visited.Add(childUrl);
 					}
-				}				
+				}
 			}
+		}
 
-			return seedNode;
+		private async Task Som()
+		{
+			BlockingCollection<PageNode> pages = new BlockingCollection<PageNode>();
+			var visited = new HashSet<string>(); //TODO: not concurrent
+
+			await Task.Run(async () =>
+			{
+				while (!pages.IsCompleted)
+				{
+					PageNode currentNode = null;
+					try
+					{
+						currentNode = pages.Take();
+					}
+					catch (InvalidOperationException) { }
+
+					if (currentNode != null)
+					{
+						var childUrls = await _urlScraper.ScrapeUrls("rootDomain", currentNode.Url);
+
+						foreach (var childUrl in childUrls)
+						{
+							if (!visited.Contains(childUrl))
+							{
+								var childNode = new PageNode(childUrl);
+								currentNode.AddChild(childNode);
+								pages.Add(childNode);
+								visited.Add(childUrl);
+							}
+						}
+					}
+				}
+			});
 		}
 	}
+
 }
